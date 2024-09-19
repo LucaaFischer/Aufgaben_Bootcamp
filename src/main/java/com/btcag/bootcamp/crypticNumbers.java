e9ca52233e9f955ade15c76b2a8bdadcb9444571
Luca Fischer <luca.fischer@btc-ag.com>
2024-09-19 14:11:55 +0200

    crypticNumbers angefangen

--- src/main/java/com/btcag/bootcamp/crypticNumbers.java
+++ src/main/java/com/btcag/bootcamp/crypticNumbers.java
@@ -1,4 +1,19 @@
 package com.btcag.bootcamp;
 
-public class crypticNumvers {
+import java.util.Scanner;
+
+public class crypticNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bitte die Zahl eingeben");
+        long zahl = input.nextLong();
+        System.out.println(zahl);
+
+        System.out.println("Bitte Passwort-Zahl eingeben");
+        long pw = input.nextLong();
+        System.out.println(pw);
+
+        long result = 0;
+    }
 }
