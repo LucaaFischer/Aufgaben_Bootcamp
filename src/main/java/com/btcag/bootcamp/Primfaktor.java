3f8dad250f6586f0c30aeb5b81a68d7017555227
Luca Fischer <luca.fischer@btc-ag.com>
2024-09-19 12:57:32 +0200

    Primzahl überarbeitet

--- src/main/java/com/btcag/bootcamp/Primfaktor.java
+++ src/main/java/com/btcag/bootcamp/Primfaktor.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class Primfaktor {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
+        System.out.println("Gib eine Zahl ein");
         long zahl = eingabe.nextLong();
         long i = 0;
+        long divider = 1;
+        String ergebnis = "Primzahl";
+
+        while (zahl % divider == 0) {
+            while (zahl != divider) {
+                ergebnis = "Keine Primzahl";
+                divider++;
+            }
+        }
+        System.out.println(ergebnis);
     }
 }
