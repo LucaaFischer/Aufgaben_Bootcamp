5a00d2dcd4976ef9639649fec2f9df71a309cb39
Luca Fischer <luca.fischer@btc-ag.com>
2024-09-19 13:11:36 +0200

    Divisionscheck überarbeitet

--- src/main/java/com/btcag/bootcamp/Divisionscheck.java
+++ src/main/java/com/btcag/bootcamp/Divisionscheck.java
@@ -7,7 +7,8 @@ public class Divisionscheck {
         Scanner eingabe_divisor = new Scanner(System.in);
         Scanner eingabe_maxZahl = new Scanner(System.in);
 
-        int i = 1;
+        int i = 0;
+        int ergebnis = 0;
 
         System.out.println("Welcher Divisor soll geprüft werden");
         int divisor = eingabe_divisor.nextInt();
@@ -15,9 +16,11 @@ public class Divisionscheck {
         int maxZahl = eingabe_maxZahl.nextInt();
 
         while (i <= maxZahl) {
-            int ergebnis = i % divisor;
+            while (i % divisor == 0) {
+                System.out.println(i);
+                i++;
+            }
             i++;
-            System.out.println(ergebnis);
         }
     }
 }
